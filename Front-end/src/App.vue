2262665149e934db00e451f61c7e56e6024f2c25
ssw02238 <ssw0223820@gmail.com>
2021-09-15 15:19:43 +0900

    FEAT: footer

--- Front-end/src/App.vue
+++ Front-end/src/App.vue
@@ -62,7 +62,21 @@
                 <h2>hi</h2>
 
     <!-- footer --> 
-    
+    <footer class="py-3">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item me-2">
+          Home 
+        </li>
+        <li class="nav-item me-2">
+          About
+        </li>
+        <li class="nav-item">
+          My Page
+        </li>
+      </ul>
+      <p class="text-center text-muted">김예찬 · 김윤서 · 방지환 · 이규정 · 정희진</p>
+      <p class="text-center text-muted">Copyright 2021 BEE ( Best ESG Enterprise ) ®All Rights Reserved</p>
+    </footer>
   </div>
 </template>
 
@@ -129,7 +143,7 @@ export default {
   padding: 5px;
 }
 
-.total-nav {
-  
+footer {
+  background-color: #FABD02;
 }
 </style>
