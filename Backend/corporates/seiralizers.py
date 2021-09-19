761f3a4f3ae453713bc35ba7fe52aa6dca05ae89
이규정 <rbwjd0101@naver.com>
2021-09-19 14:47:23 +0900

    FEAT: django serializing corporate detail & general serialinzing 추가

--- Backend/corporates/seiralizers.py
+++ Backend/corporates/seiralizers.py
@@ -25,19 +25,30 @@ class GovernanceSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ('corporate',)
 
-# 기업 Detail
-class CorporateSerializer(serializers.ModelSerializer):
-    environment_evaluation = EnvironmentSerializer(read_only=True, source="environment_set")
-    social_evaluation = SocialSerializer(read_only=True, source="social_set")
-    governance_evaluation = GovernanceSerializer(read_only=True, source="governance_set")
-    
-    class Meta:
-        model = Corporate
-        fields = ('name', 'E_rating', 'S_rating', 'G_rating', 'ESG_rating',)
-
+#기업 뉴스 
 class NewsSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = News
         fields = ('title', 'content', 'url',)
         read_only_fields = ('corporate',)
+
+# 기업 Detail
+class CorporateDetailSerializer(serializers.ModelSerializer):
+    environment_evaluation = EnvironmentSerializer(read_only=True, source="environment_set")
+    social_evaluation = SocialSerializer(read_only=True, source="social_set")
+    governance_evaluation = GovernanceSerializer(read_only=True, source="governance_set")
+    news = NewsSerializer(read_only=True, many=True, source="news_set")
+    # scrap cnt 추가해야함
+
+    class Meta:
+        model = Corporate
+        fields = ('name', 'E_rating', 'S_rating', 'G_rating', 'ESG_rating',)
+
+
+# 유사 기업 & 기업 검색 등 기업의 대략적인 정보만
+class CorporateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Corporate
+        fields = ('name', 'E_rating', 'S_rating', 'G_rating', 'ESG_rating',)
