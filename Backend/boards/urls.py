ce19ac0c736ea45649c6219cd8c88e41f590e79c
이규정 <rbwjd0101@naver.com>
2021-09-24 15:24:06 +0900

    FEAT: boards api 완료 ESG ranking을 비롯해 모든 api 구현 완료 오늘의 기업 부분 수정 예정 200개씩 보내는 데이터 수정 예정

--- Backend/boards/urls.py
+++ Backend/boards/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('esg-ranking', views.esg_ranking),
-    path('e-ranking', views.e_ranking),
-    path('s-ranking', views.s_ranking),
-    path('g-ranking', views.g_ranking),
-    path('bestcorp', views.bestcorp),
-    path('hottestcorp', views.hottestcorp),
-    path('popularcorp', views.popularcorp),
+    path('esg-ranking/', views.esg_ranking),
+    path('e-ranking/', views.e_ranking),
+    path('s-ranking/', views.s_ranking),
+    path('g-ranking/', views.g_ranking),
+    path('bestcorp/', views.bestcorp),
+    path('hottestcorp/', views.hottestcorp),
+    path('popularcorp/', views.popularcorp),
 ]
