0ec3d4b624521638466423dbf9438be3101f24fd
yachoo0611 <dpcksdl3@naver.com>
2021-09-27 14:19:46 +0900

    FEAT: RANK page fix

--- Frontend/src/views/Information/esgRank.vue
+++ Frontend/src/views/Information/esgRank.vue
@@ -1,10 +1,31 @@
 <template>
   <div>
-    
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+
+    <div class="card text-center top-card" style="width: 900px; margin: auto;">
+      <div class="card-header" style="font-size:2rem">
+        Ranks
+      </div>
+
+      <div class="card-body" >
+        <p class="card-text" style="font-size:1.5rem;">Best ESG Enterprises</p>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
+
 
 export default {
   name: 'esgRank',
