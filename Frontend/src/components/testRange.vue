23a3fa50199aa7b77e05c22787cfee2481bdcfeb
yachoo0611 <dpcksdl3@naver.com>
2021-09-28 13:33:37 +0900

    FEAT: testRange fix

--- Frontend/src/components/testRange.vue
+++ Frontend/src/components/testRange.vue
@@ -1,6 +1,116 @@
 <template>
   <div>
-    <label for="customRange3" class="form-label">질문 1</label>
+    <label for="customRange3" class="form-label">질문 1. 나는 제로 웨이스트를 실천하는 기업에 대해 2개 이상 알고 있다.</label>
+    <b-form-group label="Radios using options and slots" v-slot="{ ariaDescribedby }">
+      <b-form-radio-group
+        id="radio-slots"
+        v-model="selected"
+        :options="options"
+        :aria-describedby="ariaDescribedby"
+        name="radio-options-slots"
+      >
+        <!-- Radios in this slot will appear first -->
+        <template #first>
+          <b-form-radio value="매우 아니다">매우 아니다</b-form-radio>
+          <b-form-radio value="아니다">아니다</b-form-radio>          
+          <b-form-radio value="보통">보통</b-form-radio>
+          <b-form-radio value="그렇다">그렇다</b-form-radio>
+          <b-form-radio value="매우 그렇다">매우 그렇다</b-form-radio>
+        </template>
+
+      </b-form-radio-group>
+    </b-form-group>
+
+    <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+    <!--  -->
+        <label for="customRange3" class="form-label">질문 2. 평소 온실가스 배출량의 심각함을 인지하고 있다.</label>
+        <b-form-group label="Radios using options and slots" v-slot="{ ariaDescribedby }">
+      <b-form-radio-group
+        id="radio-slots"
+        v-model="selected"
+        :options="options"
+        :aria-describedby="ariaDescribedby"
+        name="radio-options-slots"
+      >
+        <!-- Radios in this slot will appear first -->
+        <template #first>
+          <b-form-radio value="매우 아니다">매우 아니다</b-form-radio>
+          <b-form-radio value="아니다">아니다</b-form-radio>          
+          <b-form-radio value="보통">보통</b-form-radio>
+          <b-form-radio value="그렇다">그렇다</b-form-radio>
+          <b-form-radio value="매우 그렇다">매우 그렇다</b-form-radio>
+        </template>
+
+      </b-form-radio-group>
+    </b-form-group>
+
+    <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+
+        <label for="customRange3" class="form-label">질문 3. 제공하는 제품의 품질 향상을 위해서라면 폐기물 발생량은 감수해야 한다고 생각한다.</label>
+    <input type="range" class="form-range" min="0" max="5" step="1" id="customRange3">
+    <div class="score">
+      <span>1</span>
+      <span>3</span>
+      <span>5</span>
+    </div>
+
+        <label for="customRange3" class="form-label">질문 4.</label>
+    <input type="range" class="form-range" min="0" max="5" step="1" id="customRange3">
+    <div class="score">
+      <span>1</span>
+      <span>3</span>
+      <span>5</span>
+    </div>
+
+        <label for="customRange3" class="form-label">질문 1</label>
+    <input type="range" class="form-range" min="0" max="5" step="1" id="customRange3">
+    <div class="score">
+      <span>1</span>
+      <span>3</span>
+      <span>5</span>
+    </div>
+
+        <label for="customRange3" class="form-label">질문 1</label>
+    <input type="range" class="form-range" min="0" max="5" step="1" id="customRange3">
+    <div class="score">
+      <span>1</span>
+      <span>3</span>
+      <span>5</span>
+    </div>
+
+        <label for="customRange3" class="form-label">질문 1</label>
+    <input type="range" class="form-range" min="0" max="5" step="1" id="customRange3">
+    <div class="score">
+      <span>1</span>
+      <span>3</span>
+      <span>5</span>
+    </div>
+
+        <label for="customRange3" class="form-label">질문 1</label>
+    <input type="range" class="form-range" min="0" max="5" step="1" id="customRange3">
+    <div class="score">
+      <span>1</span>
+      <span>3</span>
+      <span>5</span>
+    </div>
+
+        <label for="customRange3" class="form-label">질문 1</label>
+    <input type="range" class="form-range" min="0" max="5" step="1" id="customRange3">
+    <div class="score">
+      <span>1</span>
+      <span>3</span>
+      <span>5</span>
+    </div>
+
+        <label for="customRange3" class="form-label">질문 1</label>
+    <input type="range" class="form-range" min="0" max="5" step="1" id="customRange3">
+    <div class="score">
+      <span>1</span>
+      <span>3</span>
+      <span>5</span>
+    </div>
+
+        <label for="customRange3" class="form-label">질문 1</label>
     <input type="range" class="form-range" min="0" max="5" step="1" id="customRange3">
     <div class="score">
       <span>1</span>
@@ -8,13 +118,20 @@
       <span>5</span>
     </div>
   </div>
+
+  
 </template>
 
 <script>
 
 export default {
   name: 'test',
-}
+ data() {
+      return {
+        selected: '',
+      }
+    }
+  }
 </script>
 
 <style scoped>
--- Frontend/src/views/Information/newsList.vue
+++ Frontend/src/views/Information/newsList.vue
@@ -35,8 +35,7 @@
     </tr>
   </tbody>
 </table>
-
-    <b-table hover :items="items"></b-table>
+ 
   </div>
 </template>
 
@@ -46,27 +45,8 @@ export default {
   name: 'newsList',
   components: {
   },
-  data() {
-      return {
-        items: [
-          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          {
-            age: 89,
-            first_name: 'Geneva',
-            last_name: 'Wilson',
-            _rowVariant: 'danger'
-          },
-          {
-            age: 40,
-            first_name: 'Thor',
-            last_name: 'MacDonald',
-            _cellVariants: { age: 'info', first_name: 'warning' }
-          },
-          { age: 29, first_name: 'Dick', last_name: 'Dunlap' }
-        ]
-      }
-    }
+ 
+    
   
 }
 </script>
