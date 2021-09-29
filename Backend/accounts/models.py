5e9b104e15f00b336f70477792342c9b2df8c98a
yachoo0611 <dpcksdl3@naver.com>
2021-09-29 17:15:42 +0900

    FEAT: mbti score api 추가

--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -1,59 +1,60 @@
-from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
-from django.db import models 
-
-class UserManager(BaseUserManager):    
-    
-    use_in_migrations = True    
-    
-    def create_user(self, email, nickname, password=None):        
-        
-        if not email :            
-            raise ValueError('must have user email')        
-        user = self.model(            
-            email = self.normalize_email(email),            
-            nickname = nickname
-        )        
-        user.set_password(password)        
-        user.save(using=self._db)        
-        return user     
-        
-    def create_superuser(self, email, nickname,password):        
-       
-        user = self.create_user(            
-            email = self.normalize_email(email),            
-            nickname = nickname,            
-            password=password        
-        )        
-        user.is_admin = True        
-        user.is_superuser = True        
-        user.is_staff = True        
-        user.save(using=self._db)        
-        return user 
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
 
 
-class User(AbstractBaseUser,PermissionsMixin):    
-    
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def create_user(self, email, nickname, password=None):
+
+        if not email:
+            raise ValueError('must have user email')
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password):
+
+        user = self.create_user(
+            email=self.normalize_email(email),
+            nickname=nickname,
+            password=password
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
     objects = UserManager()
-    
-    email = models.EmailField(        
-        max_length=255,        
-        unique=True,    
-    )    
+
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
     nickname = models.CharField(
         max_length=20,
         null=False,
         unique=True
-    )     
-    is_active = models.BooleanField(default=True)    
-    is_admin = models.BooleanField(default=False)    
-    is_superuser = models.BooleanField(default=False)    
-    is_staff = models.BooleanField(default=False)     
-    date_joined = models.DateTimeField(auto_now_add=True)     
-    # mbti 필드 
-    e_score = models.FloatField(default = 0)
-    s_sore = models.FloatField(default = 0)
-    g_score = models.FloatField(default = 0)
-    USERNAME_FIELD = 'email'    
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    # mbti 필드
+    e_score = models.FloatField(default=0)
+    s_score = models.FloatField(default=0)
+    g_score = models.FloatField(default=0)
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['nickname']
 
 
--- Backend/accounts/serializers.py
+++ Backend/accounts/serializers.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -21,3 +22,12 @@ class PasswordSerializer(serializers.ModelSerializer):
         read_only_fields = ('email', 'nickname',)
 
 
+class MbtiSerializer(serializers.ModelSerializer):
+    e_score = serializers.FloatField(write_only=True)
+    s_score = serializers.FloatField(write_only=True)
+    g_score = serializers.FloatField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('e_score', 's_score', 'g_score',)
+        read_only_fields = ('email', 'nickname', 'password')
