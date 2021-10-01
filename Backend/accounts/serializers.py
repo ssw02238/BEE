ab5679b93242a074571064106ce456161cc0ea87
이규정 <rbwjd0101@naver.com>
2021-10-01 15:12:36 +0900

    DOC: update DevOps

--- Backend/accounts/serializers.py
+++ Backend/accounts/serializers.py
@@ -3,7 +3,7 @@ from django.db.models import fields
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
-from Backend.accounts.models import MBTI
+from .models import MBTI
 
 User = get_user_model()
 
